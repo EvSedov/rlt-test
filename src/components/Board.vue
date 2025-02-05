563468fbac8836d749ec826b9d360e7843f6d133
sedovev <sedovev@rgn.gov.ru>
2025-02-05 17:42:10 +0700

    adds state saving to localstorage

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {onBeforeMount, ref} from "vue";
 
 import Cell from "./Cell.vue";
 import DragElement from "./DragElement.vue";
@@ -27,11 +27,14 @@ const cells = ref<{ id: number}[]>([
 ]);
 
 const selectedItem = ref<IElement | null>(null)
-const elements = ref<IElement[]>([
+const startPositionElements = ref<IElement[]>([
   { id: 1, cellId: 1,  name: "Element 1", icon: ItemFirst, description: 'Описание предмета с названием Element 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tempore!', count: 4 },
   { id: 2, cellId: 2,  name: "Element 2", icon: ItemSecond, description: 'Описание предмета с названием Element 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tempore!', count: 2 },
   { id: 3, cellId: 3,  name: "Element 3", icon: ItemThird, description: 'Описание предмета с названием Element 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, tempore!', count: 5 },
 ]);
+const localStorageKey = 'currentPositionElements';
+
+const currentPositionElements = ref<IElement[]>([]);
 
 
 
@@ -48,14 +51,21 @@ const startDragElement = (event: DragEvent, element: IElement) => {
 }
 
 const dropElement = (elementId: number, cellId: number) => {
-  elements.value = elements.value.map((item: IElement) => {
+  currentPositionElements.value = currentPositionElements.value.map((item: IElement) => {
     if (item.id === elementId) {
       item.cellId = cellId;
     }
     return item;
-  })
+  });
+  localStorage.setItem(localStorageKey, JSON.stringify(currentPositionElements.value));
   console.log({elementId, cellId});
 }
+
+onBeforeMount(() => {
+  currentPositionElements.value = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+  currentPositionElements.value = currentPositionElements.value.length ? currentPositionElements.value : startPositionElements.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(currentPositionElements.value));
+});
 
 </script>
 
@@ -69,7 +79,7 @@ const dropElement = (elementId: number, cellId: number) => {
         @drop-cell="(elementId: number) => dropElement(elementId, cell.id)"
       >
         <DragElement
-            v-for="element in elements.filter((el) => el?.cellId === cell.id)"
+            v-for="element in currentPositionElements.filter((el: IElement) => el?.cellId === cell.id)"
             :id="element.id"
             :srcIcon="element.icon"
             :name="element.name"
